0040c195d2f976ce18f33909678e58a0e16b86c3
Faysal Developer <apple@MacBook-Pro.local>
2023-02-09 07:19:39 +0600

    Dependence Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useRef, useState } from "react";
+import { Container } from "react-bootstrap";
+import { Camera } from "react-camera-pro";
+
 
 
 function App() {
+  const camera = useRef(null)
+  const [image, setImage] = useState(null)
   return (
     <div>
-      
+       <Container>
+       <Camera ref={camera} />
+       </Container>
+       <button onClick={() => {setImage(camera.current.takePhoto())}}>Take Photo</button>
+       <img src={image} alt="Take Photos" />
     </div>
   );
 }
