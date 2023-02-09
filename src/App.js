d32cd3aeb5e4e33a3c26b503c9930a556e83c958
mdfaysal22 <mdfaysal.freelancer@gmail.com>
2023-02-09 20:11:41 +0600

    Container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import { useRef, useState } from "react";
-import { Container } from "react-bootstrap";
-import { Camera } from "react-camera-pro";
-
-
+import { Button, Container, Navbar } from "react-bootstrap";
+import { IoIosArrowBack } from "react-icons/io";
+import './App.css';
 
 function App() {
-  const camera = useRef(null)
-  const [image, setImage] = useState(null)
   return (
     <div>
-       <Container>
-       <Camera ref={camera} />
-       </Container>
-       <button onClick={() => {setImage(camera.current.takePhoto())}}>Take Photo</button>
-       <img src={image} alt="Take Photos" />
+      <Navbar>
+        <Container>
+          <Navbar.Brand>My-Camera-app</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <Button variant="outline-dark">
+                <IoIosArrowBack></IoIosArrowBack>
+              </Button>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="container-sm mx-auto camera-container">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reprehenderit minus, ipsam eligendi est voluptatibus vel. Alias ipsum tempore eos?</h1>
+
+      </div>
     </div>
   );
 }
